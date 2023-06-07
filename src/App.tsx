16dafbadb6f79f1fbbf320013bf1f31f5b6d8ec3
Viktoriyadegt <vika.degtyareva.12@mail.ru>
2023-06-07 19:29:06 +0300

    todolist_props

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,33 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
+
+export type TaskPropsType = {
+    id: number
+    title: string
+    isDone: boolean
+}
 
 function App() {
+
+const tasks1: Array<TaskPropsType> = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false }
+    ]
+    const tasks2: Array<TaskPropsType> = [
+        { id: 1, title: "Hello world", isDone: true },
+        { id: 2, title: "I am Happy", isDone: false },
+        { id: 3, title: "Yo", isDone: false }
+    ]
+
     return (
         <div className="App">
-            <Todolist/>
-            <Todolist/>
-            <Todolist/>
+            <Todolist title={'What to learn'} tasks={tasks1}/>
+            <Todolist title={'Songs'} tasks={tasks2}/>
         </div>
     );
 }
 
 export default App;
 
-export function Todolist() {
-    return (
-        <div>
-            <h3>What to learn</h3>
-            <div>
-                <input/>
-                <button>+</button>
-            </div>
-            <ul>
-                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                <li><input type="checkbox" checked={false}/> <span>React</span></li>
-            </ul>
-            <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
-            </div>
-        </div>
-    );
-}
